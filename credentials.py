39340d091f7fa180f5eecf4762982f93bd72e1b5
NALM98 <n.myagkov@mail.ru>
2018-04-28 10:25:56 +0300

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'YAH6ftaQFgANdPwqU1Mzw5FEQ'
+consumer_secret = 'w7MsCQlUPMFqHrH5zMn64T7FRDaj4fjcEAc2tW3QGRhO7X32su'
+access_token = '881189337920135169-Xx30kaHBPuCcjWTw2dXnkrio8OcZNxM'
+access_token_secret = '1onK8PJNMla04l3vl6yPFBc1DUpLVGWgcMNmsrDNRZqAa'
